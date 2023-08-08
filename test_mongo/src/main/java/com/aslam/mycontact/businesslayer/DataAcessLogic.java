70e8b8f1cd46deacb500d8d09aa29fa9e68c3d7d
aslamcontact <aslam.mycontact@gmail.com>
2023-08-09 00:45:28 +0530

    add method to access product dcument and create method for make product

--- test_mongo/src/main/java/com/aslam/mycontact/businesslayer/DataAcessLogic.java
+++ test_mongo/src/main/java/com/aslam/mycontact/businesslayer/DataAcessLogic.java
@@ -2,7 +2,9 @@ package com.aslam.mycontact.businesslayer;
 
 import com.aslam.mycontact.daolayer.catelog.*;
 import com.aslam.mycontact.daolayer.catelog.Pricing.Price;
+import com.aslam.mycontact.daolayer.catelog.Pricing.PriceV1;
 import com.aslam.mycontact.daolayer.catelog.quantity.NosQuantityV1;
+import com.aslam.mycontact.daolayer.catelog.quantity.QuantityV1;
 import com.aslam.mycontact.daolayer.catelog.variation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,67 +16,77 @@ public class DataAcessLogic {
     @Autowired
     private ProductRepository productRepository;
 
-    public void productTest2(){
-        //shirt with NONE Variation
-        Product shirt=new Product("Roadster");
-        Price price=new Price(550D);
-        shirt.setProductDescription(List.of("wash able","no hand wash"));
-        NosQuantityV1 shirtQuqntity=new NosQuantityV1(5L,price);
-        shirt.setQuantityAndPrice(shirtQuqntity);
-// shirt with singleVar
-        Product shirtOtto=new Product("Otto");
-        shirtOtto.setProductDescription(List.of("Otto Brand","No Fad "));
+    public Optional<Product> createProduct( String productName,
+                               String productBrand,
+                               List<String> descriptions,
+                               Double price,
+                               Long quantity) {
 
-        SingleVariation ottoShirtSize=new SingleVariation("Size");
-        NosQuantityV1 lSize=new NosQuantityV1(11L,new Price(500.5));
-        NosQuantityV1 sSize=new NosQuantityV1(6L,new Price(3458.6));
-        NosQuantityV1 mSize=new NosQuantityV1(3L,new Price(355.6));
+           productRepository
+                   .findById("Id_"+productName.toLowerCase()+productBrand.toLowerCase())
+                     .ifPresent(res-> {
 
-        ottoShirtSize.setVariations(Map.of(
-                "L",lSize,
-                "S",sSize,
-                "M",mSize
-        ));
-       shirtOtto.setVariation(ottoShirtSize);
+                            throw new IllegalStateException("Use Another product and brand name"
+                                        +res.getProductName());
+                                       }
+                                  );
 
-       //shitr for Dduble variation
-
-        Product shirtAdidas=new Product("Adidas");
-        shirtOtto.setProductDescription(List.of("Adidas  Brand","High Quality "));
-        DoubleVariation colorSize=new DoubleVariation("Color");
-
-        SingleVariation redColor=new SingleVariation("Size");
-        redColor.setVariations(Map.of(
-                 "L",new  NosQuantityV1(15L,new Price(50.5)),
-                 "M",new NosQuantityV1(65L,new Price(3458.6)),
-                 "S",new NosQuantityV1(34L,new Price(355.6))
-        ));
-
-        SingleVariation blueColor=new SingleVariation("Size");
-        blueColor.setVariations(Map.of(
-                "L",new  NosQuantityV1(13L,new Price(50.5)),
-                "M",new NosQuantityV1(7L,new Price(3.6)),
-                "S",new NosQuantityV1(1L,new Price(23.6))
-        ));
+          Product product=newProduct( productName,
+                                      productBrand,
+                                      descriptions
+                                     );
+          product.setQuantityAndPrice(newQuantity(newPrice(price),quantity));
 
 
-        colorSize.setVariations(Map.of("red",redColor,"blue",blueColor));
-        shirtAdidas.setVariation(colorSize);
-
-
-
-        System.out.println(shirt.getQuantityAndPrice().get().toString());
-        System.out.println(shirtOtto.getQuantityAndPrice().get().toString());
-        System.out.println(shirtAdidas.getQuantityAndPrice().get().toString());
-        productRepository.save(shirt);
-        productRepository.save(shirtOtto);
-        productRepository.save(shirtAdidas);
-
-
-
-
-
+                        return Optional.of(productRepository.save(product));
 
 
     }
+    private Product newProduct(String name,String brand,List<String> descriptions)
+    {
+        Product newProduct=new Product(name,brand);
+        newProduct.setProductDescription(descriptions);
+
+        return newProduct;
+    }
+
+    private PriceV1 newPrice(Double price){
+        return new Price(price);
+    }
+
+    private QuantityV1 newQuantity(PriceV1 price,Long quantity)
+    {
+        return new NosQuantityV1(quantity,price);
+    }
+
+   private SingleVariation newSingleVariation(String name)
+    {
+        SingleVariation variation=new SingleVariation();
+        variation.setName(name);
+        return variation;
+    }
+    private DoubleVariation newDoubleVariation(String name)
+    {
+         DoubleVariation variation=new DoubleVariation();
+        variation.setName(name);
+        return variation;
+    }
+    private VariationV1<?> updateVariation(String name, Map<String,QuantityV1> variations)
+    {
+        SingleVariation singleVariation=newSingleVariation(name);
+        singleVariation.setVariations(variations);
+        return singleVariation;
+
+    }
+
+    private VariationV1<?> updateVariation(Map<String,SingleVariation> variations,String mainName)
+    {
+        DoubleVariation doubleVariation=newDoubleVariation(mainName);
+        doubleVariation.setVariations(variations);
+        return doubleVariation;
+
+    }
+
+
+
 }
