75fed1b54f85ab426af1407ee6b236edc9b2777f
aslamcontact <aslam.mycontact@gmail.com>
2023-08-10 23:32:50 +0530

    testing purpose

--- test_mongo/src/main/java/com/aslam/mycontact/daolayer/catelog/quantity/QuantityV1.java
+++ test_mongo/src/main/java/com/aslam/mycontact/daolayer/catelog/quantity/QuantityV1.java
@@ -2,7 +2,9 @@ package com.aslam.mycontact.daolayer.catelog.quantity;
 
 import com.aslam.mycontact.daolayer.catelog.Pricing.PriceV1;
 
-public interface QuantityV1 {
+import java.util.function.Function;
+
+public interface QuantityV1  {
     public Long getQuantity() ;
     public PriceV1 getPrice();
 
